f92d364cb2386861476b78ca0eda0130e718d4c5
Briyankan23 <briyankanni@gmail.com>
2026-07-16 12:34:37 +0000

    change id code

--- Basic/id.c
+++ Basic/id.c
@@ -4,16 +4,16 @@ int main(){
 	int h,s;
 
 	printf("Enter the employee ID:");
-	scanf("%s",&id);
-	printf("Enter  the working hours per day:\n");
+	scanf("%s",&id[10]);
+	printf("Enter  the working hours per day:");
 	scanf("%d",&h);
 	printf("Enter the salary per hour:");
 	scanf("%d",&s);
-	printf("\n\n");
+	printf("\n");
 
 	printf("==========================\n");
 	printf("Employee Id:%s\n",id);
-	printf(" The monthly salary:%d\n",h*s*30);
+	printf("The monthly salary:%d\n",h*s*30);
 
 
 }
