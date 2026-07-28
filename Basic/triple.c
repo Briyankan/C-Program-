e4a9814a944d1dc25ebff8f4d05095e481114702
Briyankan23 <briyankanni@gmail.com>
2026-07-28 10:27:32 +0000

    triple

---
+++ Basic/triple.c
@@ -0,0 +1,34 @@
+/*
+Populate an array where each element is triple the previous
+
+Write a C program to read and print the elements of an array with length 7. Before printing, insert the triple of the previous position, starting from the second position.
+For example, if the first number is 2, the array numbers must be 2, 6, 18, 54 and 162
+Test Data:
+Input the first number of the array: 5
+Expected Output:
+n[0] = 5
+n[1] = 15
+n[2] = 45
+n[3] = 135
+n[4] = 405
+*/
+
+#include<stdio.h>
+int main(){
+    int n[5],i,x;
+    printf("Populate an array where each element is triple the previous\n");
+    printf("-----------------------------------------------------------\n");
+
+    printf("Input the first nuumber of array : ");
+    scanf("%d",&x);
+
+    for(i=0;i<5;i++){
+        n[i] = x;
+        x = x * 3;
+
+    }
+    printf("Output\n");
+    for(i=0;i<5;i++){
+        printf("n[%d] = %d\n",i,n[i]);
+    }
+}
