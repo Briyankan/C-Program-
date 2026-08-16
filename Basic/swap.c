e56b9c4ea5ffac80392c50733e4ed0dfab462595
Briyankan23 <briyankanni@gmail.com>
2026-08-16 23:21:37 +0000

    17 aug

---
+++ Basic/swap.c
@@ -0,0 +1,21 @@
+/*Write a C program that swaps two numbers without using a third variable.
+Input value for x & y:
+Before swapping the value of x & y: 5 7
+After swapping the value of x & y: 7 5 */
+
+#include<stdio.h>
+int main(){
+    printf("Swap two number \n");
+    printf("----------------\n");
+
+    int x,y;
+    printf("Input value for x & y : ");
+    scanf("%d %d",&x,&y);
+    printf("Before swapping the value of x & y : %d , %d\n",x,y);
+
+    x = x + y;
+    y = x - y;
+    x = x - y;
+
+    printf("After swapping the value of x & y : %d , %d\n",x,y);
+}
